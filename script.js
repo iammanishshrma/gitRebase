1c1028296a5c37c59a39ce2e43d39966a3ab2c9b
Manish <iammanishshrma@gmail.com>
2022-08-20 02:04:06 +0530

    squashed2

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ import React from "react";
 
 const script = () => {
   const test = () => {
-    console.log();
-    console.log();
+    console.log("1");
+    alert(0);
+    console.log("2");
+    alert(1);
+    console.log("3");
+    alert(2);
+    console.log("4");
+  };
+  const trying = () => {
+    console.log("trying");
+  };
+  const test2 = () => {
+    console.log("1");
+    console.log("2");
+    console.log("3");
+    console.log("4");
   };
   return <div>script</div>;
 };
