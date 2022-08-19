f917630ce7492d6ac0335282423a15d91c9b7f08
Manish <iammanishshrma@gmail.com>
2022-08-20 02:15:29 +0530

    added test2 function

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const script = () => {
     console.log("3");
     console.log("4");
   };
+  const test2 = () => {
+    console.log("1");
+    console.log("2");
+    console.log("3");
+    console.log("4");
+  };
   return <div>script</div>;
 };
 
